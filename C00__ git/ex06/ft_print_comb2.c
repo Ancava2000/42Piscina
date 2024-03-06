811cde564da4cc4cd273bdcfb67479d43fdcb4a2
Ancava2000 <160865430+Ancava2000@users.noreply.github.com>
2024-03-06 15:15:42 +0100

    Add files via upload
    C00

---
+++ C00__ git/ex06/ft_print_comb2.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acarro-v <acarro-v@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/02 09:56:50 by acarro-v          #+#    #+#             */
+/*   Updated: 2024/03/04 13:18:37 by acarro-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	k;
+
+	i = 0;
+	k = 0;
+	while (i < 100)
+	{
+		k = i + 1;
+		while (k < 100)
+		{
+			ft_putchar(i / 10 + '0');
+			ft_putchar(i % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(k / 10 + '0');
+			ft_putchar(k % 10 + '0');
+			if ((i / 10 != 9) || (i % 10 != 8))
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			k++;
+		}
+		i++;
+	}
+}
